5198a987ed00952faa118e04350c8c4b119e06f2
aobjsvue <andres.orta@hotmail.com>
2022-06-14 19:18:52 +0200

    fewupdates

--- Nivel1/js/checkout.js
+++ Nivel1/js/checkout.js
@@ -1,28 +1,101 @@
-
-// Exercise 6
+// Exercise 7
 function validate() {
 	var error = 0;
 	// Get the input fields
 	var fName = document.getElementById("fName");
 	var fEmail = document.getElementById("fEmail");
+	var fAddress = document.getElementById("fAddress");
+	var fLastN = document.getElementById("fLastN");
+	var fPassword = document.getElementById("fPassword");
+	var fPhone = document.getElementById("fPhone");
 
 	// Get the error elements
 	var errorName = document.getElementById("errorName");
-	var errorEmail = document.getElementById("errorEmail");  
-	
+	var errorEmail = document.getElementById("errorEmail");
+	var errorAddress = document.getElementById("errorAddress");
+	var errorLastN = document.getElementById("errorLastN");
+	var errorPassword = document.getElementById("errorPassword");
+	var errorPhone = document.getElementById("errorPhone");
+
 	// Validate fields entered by the user: name, phone, password, and email
-	if(fName.value == ""){
+	let fNameLetters = onlyLettersSpaces(fName.value);
+	if(fName.value == "" || fName.value.length < 3 || !fNameLetters){
+		errorName.style.display = "block";
+		fName.style.borderColor = "#dc3545";
 		error++;
+	} else {
+		errorName.style.display = "none";
+		fName.style.borderColor = "#198754";
 	}
 
-	if(fEmail.value == ""){
+	let fEmailValid = emailRegEx(fEmail.value);
+	if(fEmail.value == "" || fEmail.value.length < 3 || !fEmailValid){
+		errorEmail.style.display = "block";
+		fEmail.style.borderColor = "#dc3545";
 		error++;
+	} else {
+		errorEmail.style.display = "none";
+		fEmail.style.borderColor = "#198754";
 	}
-	 
+
+	if(fAddress.value == "" || fAddress.value.length < 3){
+		errorAddress.style.display = "block";
+		fAddress.style.borderColor = "#dc3545";
+		error++;
+	} else {
+		errorAddress.style.display = "none";
+		fAddress.style.borderColor = "#198754";
+	}
+
+	let fLastNLetters = onlyLettersSpaces(fLastN.value);
+	if(fLastN.value == "" || fLastN.value.length < 3 || !fLastNLetters){
+		errorLastN.style.display = "block";
+		fLastN.style.borderColor = "#dc3545";
+		error++;
+	} else {
+		errorLastN.style.display = "none";
+		fLastN.style.borderColor = "#198754";
+	}
+
+	let fPasswordValid = passwordRegEx(fPassword.value);
+	if(fPassword.value == "" || !fPasswordValid){
+		errorPassword.style.display = "block";
+		fPassword.style.borderColor = "#dc3545";
+		error++;
+	} else {
+		errorPassword.style.display = "none";
+		fPassword.style.borderColor = "#198754";
+	}
+
+	let fPhoneValid = phoneRegEx(fPhone.value);
+	if(fPhone.value == "" || !fPhoneValid){
+		errorPhone.style.display = "block";
+		fPhone.style.borderColor = "#dc3545";
+		error++;
+	} else {
+		errorPhone.style.display = "none";
+		fPhone.style.borderColor = "#198754";
+	}
+	
 	if(error>0){
-		alert("Error");
+		alert('One or more fields are not valid. Please, check it and click on "Save" again');
 	}else{
-		alert("OK");
+		alert("Continue to payment method");
 	}
+}
 
+function onlyLettersSpaces(str) {
+	return /^[a-zA-Z\s]+$/.test(str);
+}
+
+function emailRegEx(str) {
+	return /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(str);
+}
+
+function passwordRegEx(str) {
+	return /^[0-9a-zA-Z]\w{3,7}$/.test(str);
+}
+
+function phoneRegEx(str) {
+	return /^\d{9}$/.test(str);
 }
--- Nivel1/js/shop.js
+++ Nivel1/js/shop.js
@@ -2,7 +2,7 @@
 var products = [
     {
         id: 1,
-        name: 'cooking oil',
+        name: 'Cooking oil',
         price: 10.5,
         type: 'grocery',
         offer: {
@@ -75,33 +75,43 @@ var total = 0;
 function buy(id) {
     // 1. Loop for to the array products to get the item to add to cart
     // 2. Add found product to the cartList array
-    // La funció que has de completar es diu buy(), la qual rep l'id del producte a afegir. Has de buscar el producte utilitzant aquest id rebut a l'array products, per finalment afegir-ho al array cartList. Els botons que han d'executar la funció buy() són els blaus que es troben en cada producte. Ajuda: per a buscar el producte a l'array products mitjançant l'id, pots utilitzar un bucle for.
+    
     for (let i = 0; i < products.length; i++) {
         if (products[i].id === id) {
             cartList.push(products[i]);
-            i++;
         }
     }
-    console.log("productos añadidos a cartList debajo:");
+
+    let countProduct = cartList.length;
+    document.getElementById("count_product").innerHTML = countProduct;
+
     console.log(cartList);
 }
 
 // Exercise 2
 function cleanCart() {
-    // Ara implementarem una funció que permeti a l'usuari/ària eliminar l'array generat a l'anterior exercici: buidar el carret. En aquest cas, hauràs d'emplenar la funció cleanCart(), la qual ha de reinicialitzar la variable cartList.
     cartList = [];
-    console.log("cartList vacía " + cartList);
+    document.getElementById("count_product").innerHTML = 0;
+
+    total = 0;
+
+    cart = [];
+    document.getElementById("cart_list").innerHTML = "";
+    document.getElementById("total_price").innerHTML = total.toFixed(2);
 }
 
 // Exercise 3
 function calculateTotal() {
     // Calculate total price of the cart using the "cartList" array
-    //Genial, l'e-commerce va prenent forma!, és el moment de calcular el total de l'import del carretó. S'ha d'implementar un bucle for per anar sumant l'import de tots els productes.
-    // let prices = 0;
+    
     for (let i = 0; i < cartList.length; i++) {
         total += cartList[i].price;
     }
-    return console.log("precio total de la suma de productos: " + total);
+
+    for (let i = 0; i < cart.length; i++) {
+        total += cart[i].price * cart[i].quantity;
+    }
+    return console.log(total);
 }
 
 // Exercise 4
@@ -133,9 +143,10 @@ function generateCart() {
                     repeated = false;
                 }
             }
+            // Terminate outer loop
         }
 
-        if (repeated == false && i != 0) {
+        if (!repeated && i != 0) {
             cartList[i].quantity = 1;
             cart.push(cartList[i]);
         }
@@ -146,24 +157,69 @@ function generateCart() {
 // Exercise 5
 function applyPromotionsCart() {
     // Apply promotions to each item in the array "cart"
+    // El client ens ha transmès dos tipus de promocions que vol per a la seva e-commerce: Si l'usuari compra 3 o més ampolles d'oli, el preu del producte descendeix a 10 euros. Quan es compren 10 o més productes per a fer pastís, el seu preu es rebaixa a 2/3. En aquest exercici has de completar la funció applyPromotionsCart(), la qual rep l'array cart, modificant el camp subtotalWithDiscount en cas que s'apliqui la promoció. Ajuda: com que producte del carret té quantitat, ja pots validar si té descompte: En cas que un producte tingui descompte, s'ha de guardar el preu total amb descompte en el camp: subtotalWithDiscount. Si no s'ha d'aplicar descompte, no fa falta que guardis res.
+    for (let i = 0; i < cart.length; i++) {
+        if (cart[i].id == 1) {
+            if (cart[i].quantity >= 3) {
+                cart[i].price = 10;
+                subtotalWithDiscount = cart[i].price * cart[i].quantity;
+            } else {
+                cart[i].price = 10.5;
+            }
+        }
+        if (cart[i].id == 3) {
+            if (cart[i].quantity >= 10) {
+                cart[i].price = 3.33;
+                subtotalWithDiscount = cart[i].price * cart[i].quantity;
+            } else {
+                cart[i].price = 5;
+            }
+        }
+    }
 }
 
 // Exercise 6
 function printCart() {
     // Fill the shopping cart modal manipulating the shopping cart dom
+    //El codi encarregat de mostrar el carret de la compra en el modal amb id "cartModal", ha d'incloure's dins de la funció printCart()
+    let cartTable = "";
+
+    for (let i = 0; i < cart.length; i++) {
+        cartTable += "<tr>";
+		cartTable += '<th scope="row">';
+        cartTable += cart[i].name;
+        cartTable += "</th>";
+		cartTable += "<td>";
+        cartTable += "$" + cart[i].price;
+        cartTable += "</td>";
+		cartTable += "<td>";
+        cartTable += cart[i].quantity;
+        cartTable += "</td>";
+		cartTable += "<td>";
+        cartTable += "$" + (cart[i].price * cart[i].quantity).toFixed(2);
+        cartTable += "</td>";
+        cartTable += "</tr>";
+    }
+
+    for (let i = 0; i < cart.length; i++) {
+        total += cart[i].price * cart[i].quantity;
+    }
+    
+    document.getElementById("cart_list").innerHTML = cartTable;
+    document.getElementById("total_price").innerHTML = total.toFixed(2);
 }
 
 
 // ** Nivell II **
 
-// Exercise 7
+// Exercise 8
 function addToCart(id) {
     // Refactor previous code in order to simplify it 
     // 1. Loop for to the array products to get the item to add to cart
     // 2. Add found product to the cart array or update its quantity in case it has been added previously.
 }
 
-// Exercise 8
+// Exercise 9
 function removeFromCart(id) {
     // 1. Loop for to the array products to get the item to add to cart
     // 2. Add found product to the cartList array
